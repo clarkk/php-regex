b7cb82c08efc4231463310e0aaa61a9a4a2d8de2
clarkk <clark@electrobeat.dk>
2022-10-08 12:18:54 +0200

    cleaning

--- Pattern.php
+++ Pattern.php
@@ -7,6 +7,6 @@ class Pattern {
 	const PATTERN_MONETARY_AMOUNT_STRICT 	= '/^'.self::MONETARY_AMOUNT_WHOLE.'[.,]\d{2}$/';
 	const PATTERN_MONETARY_AMOUNT 			= '/^'.self::MONETARY_AMOUNT_WHOLE.'(?:[.,]\d{1,2})?$/';
 	
-	private const DATE_SEPARATOR 					= '[\-.\/]';
+	private const DATE_SEPARATOR 			= '[\-.\/]';
 	const PATTERN_DATE 						= '/^(?:\d{2}'.self::DATE_SEPARATOR.'){2}(?:\d{2}|\d{4})|\d{4}(?:'.self::DATE_SEPARATOR.'\d{2}){2}$/';
 }

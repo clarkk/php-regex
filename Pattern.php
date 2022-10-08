e70b02d95aa37d4e74b0d1aa0d10d8015cdf6772
clarkk <clark@electrobeat.dk>
2022-10-08 12:06:54 +0200

    patterns as public

--- Pattern.php
+++ Pattern.php
@@ -3,10 +3,10 @@
 namespace Regex;
 
 class Pattern {
-	private const MONETARY_AMOUNT_WHOLE 			= '-?(?:0|[1-9]\d{0,2}(?:[.,]?\d{3})*)';
-	protected const MONETARY_AMOUNT_PATTERN_STRICT 	= '/^'.self::MONETARY_AMOUNT_WHOLE.'[.,]\d{2}$/';
-	protected const MONETARY_AMOUNT_PATTERN 		= '/^'.self::MONETARY_AMOUNT_WHOLE.'(?:[.,]\d{1,2})?$/';
+	private const MONETARY_AMOUNT_WHOLE 	= '-?(?:0|[1-9]\d{3,}|[1-9]\d{0,2}(?:([,.])\d{3}(?:\1\d{3})*)?)';
+	const PATTERN_MONETARY_AMOUNT_STRICT 	= '/^'.self::MONETARY_AMOUNT_WHOLE.'[.,]\d{2}$/';
+	const PATTERN_MONETARY_AMOUNT 			= '/^'.self::MONETARY_AMOUNT_WHOLE.'(?:[.,]\d{1,2})?$/';
 	
 	private const DATE_SEPARATOR 					= '[\-.\/]';
-	protected const DATE_PATTERN 					= '/^(?:\d{2}'.self::DATE_SEPARATOR.'){2}(?:\d{2}|\d{4})|\d{4}(?:'.self::DATE_SEPARATOR.'\d{2}){2}$/';
+	const PATTERN_DATE 						= '/^(?:\d{2}'.self::DATE_SEPARATOR.'){2}(?:\d{2}|\d{4})|\d{4}(?:'.self::DATE_SEPARATOR.'\d{2}){2}$/';
 }
